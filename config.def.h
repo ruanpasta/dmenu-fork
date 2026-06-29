41728de027e3189a0bbbceb2a2392d959bc5874a
Ruan Pasta <ruan_augusto_pasta@hotmail.com>
2026-06-28 21:14:58 -0300

    apply center patch

--- config.def.h
+++ config.def.h
@@ -6,6 +6,8 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static const char *fonts[] = {
 	"monospace:size=10"
 };
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
